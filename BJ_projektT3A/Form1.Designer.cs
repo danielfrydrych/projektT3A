591e3a5f1d5a1d18478678ad951f08f339ef1a13
danielfrydrych <danielfrydrych44@gmail.com>
2026-03-04 09:11:27 +0100

    test

--- BJ_projektT3A/Form1.Designer.cs
+++ BJ_projektT3A/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            Hit = new Button();
+            Stand = new Button();
+            KartyD = new Label();
+            KartyH = new Label();
+            SuspendLayout();
+            // 
+            // Hit
+            // 
+            Hit.Location = new Point(275, 335);
+            Hit.Name = "Hit";
+            Hit.Size = new Size(67, 49);
+            Hit.TabIndex = 0;
+            Hit.Text = "Hit";
+            Hit.UseVisualStyleBackColor = true;
+            // 
+            // Stand
+            // 
+            Stand.Location = new Point(380, 335);
+            Stand.Name = "Stand";
+            Stand.Size = new Size(67, 49);
+            Stand.TabIndex = 2;
+            Stand.Text = "Stand";
+            Stand.UseVisualStyleBackColor = true;
+            // 
+            // KartyD
+            // 
+            KartyD.AutoSize = true;
+            KartyD.Location = new Point(357, 88);
+            KartyD.Name = "KartyD";
+            KartyD.Size = new Size(13, 15);
+            KartyD.TabIndex = 3;
+            KartyD.Text = "0";
+            // 
+            // KartyH
+            // 
+            KartyH.AutoSize = true;
+            KartyH.Location = new Point(357, 205);
+            KartyH.Name = "KartyH";
+            KartyH.Size = new Size(13, 15);
+            KartyH.TabIndex = 4;
+            KartyH.Text = "0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(KartyH);
+            Controls.Add(KartyD);
+            Controls.Add(Stand);
+            Controls.Add(Hit);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button Hit;
+        private Button Stand;
+        private Label KartyD;
+        private Label KartyH;
     }
 }
